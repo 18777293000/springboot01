bfec77570b640273789d8c736a86827c01e58ffa
yangming <120953990@qq.com>
2021-03-03 17:54:51 +0800

    新增接口路径

--- src/main/java/com/atguigu/springboot/Application.java
+++ src/main/java/com/atguigu/springboot/Application.java
@@ -1,9 +1,17 @@
 package com.atguigu.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 /**
  * @创建人 ym
  * @创建时间 2021/3/3 0003
  * @描述
  **/
+@SpringBootApplication
 public class Application {
+    public static void main(String[] arge){
+         ConfigurableApplicationContext run = SpringApplication.run(Application.class, arge);
+    }
 }
