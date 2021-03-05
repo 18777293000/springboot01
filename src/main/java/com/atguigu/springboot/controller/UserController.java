5ead73afc8d7568f0d819aeab6fe564546a876d1
yangming <120953990@qq.com>
2021-03-05 17:57:45 +0800

    更新swagger配置信息

--- src/main/java/com/atguigu/springboot/controller/UserController.java
+++ src/main/java/com/atguigu/springboot/controller/UserController.java
@@ -6,6 +6,7 @@ import com.atguigu.springboot.query.UserQuery;
 import com.atguigu.springboot.service.UserService;
 import com.atguigu.springboot.vo.User;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -16,23 +17,40 @@ import javax.annotation.Resource;
  * @描述
  **/
 @RestController
+@Api(tags = "用户模块管理")
 public class UserController {
 
     @Resource
     private UserService userService;
 
     @GetMapping("user/uname/{userName}")
+    @ApiOperation(value="用户模块-根据用户名查询用户记录")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userName", value="用户名", required = true, paramType = "String")
+    })
+    @ApiResponses({
+            @ApiResponse(code=300, message="参数错误"),
+            @ApiResponse(code=400, message="路由错误,都是随便写的")
+    })
     public User queryUserByUserName( @PathVariable("userName") String userName ){
         return userService.queryUserByName(userName);
     }
 
     @GetMapping("user/uId/{userId}")
+    @ApiOperation(value="用户模块-根据用户ID查询用户记录")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value="用户ID", required = true, paramType = "Path")
+    })
     public User queryUserById(@PathVariable Integer userId){
         return userService.queryUserById(userId);
     }
 
     @PutMapping(value = "user", consumes = "application/json")
     @ResponseBody
+    @ApiOperation(value="用户模块-新增用户")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "user", value = "用户", required = true, paramType = "User")
+    })
     public ResultInfo saveUser(@RequestBody User user){
         System.out.println(user.getId());
         System.out.println(user.getName());
@@ -55,6 +73,10 @@ public class UserController {
     }
 
     @PostMapping("user")
+    @ApiOperation(value="用户模块-更新用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "user", value = "用户", required = true, paramType = "User")
+    })
     public ResultInfo updateUser(@RequestBody User user){
         //saveuser里面没有返回值，所以不用像上面两个一样要return，但是要做返回错误捕捉
         //自定义错误类，来自定义返回的错误对象
@@ -74,6 +96,10 @@ public class UserController {
     }
 
     @DeleteMapping("user/del/{userId}")
+    @ApiOperation(value="用户模块-删除用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "用户ID", required = true, paramType = "Integer")
+    })
     public ResultInfo delete(@PathVariable Integer userId){
         //saveuser里面没有返回值，所以不用像上面两个一样要return，但是要做返回错误捕捉
         //自定义错误类，来自定义返回的错误对象
@@ -93,6 +119,10 @@ public class UserController {
     }
 
     @GetMapping("user/list")
+    @ApiOperation(value="用户模块-获取用户列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userQuery", value = "列表参数", required = true, paramType = "UserQuery")
+    })
     public PageInfo<User> queryUserByParams(UserQuery userQuery){
         return userService.queryUserByParams(userQuery);
     }
--- src/main/java/com/atguigu/springboot/vo/User.java
+++ src/main/java/com/atguigu/springboot/vo/User.java
@@ -1,15 +1,25 @@
 package com.atguigu.springboot.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * @创建人 ym
  * @创建时间 2021/3/4 0004
  * @描述
  **/
+
+@ApiModel(description = "用户实体对象")
 public class User {
+
+    @ApiModelProperty(value = "用户ID")
     private Integer id;
+
+    @ApiModelProperty(value = "用户民")
     private String userName;
+
+    @ApiModelProperty(value = "用户密码")
     private Integer userPwd;
 
     public User(){}
