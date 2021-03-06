8daa07f29b1af06b96d5d4264dadbda6f81a8e2a
yangming <120953990@qq.com>
2021-03-06 15:11:30 +0800

    application入口添加开启缓存

--- src/main/java/com/atguigu/springboot/Application.java
+++ src/main/java/com/atguigu/springboot/Application.java
@@ -3,6 +3,7 @@ package com.atguigu.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
@@ -11,7 +12,10 @@ import org.springframework.context.ConfigurableApplicationContext;
  * @描述
  **/
 @SpringBootApplication
+//自定义扫描包路径
 @MapperScan("com.atguigu.springboot.dao")
+//开启缓存应用
+@EnableCaching
 public class Application {
     public static void main(String[] arge){
          ConfigurableApplicationContext run = SpringApplication.run(Application.class, arge);
